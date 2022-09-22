a23c2b3d6477f83b5060790e8b56367d271826cd
Marcus <marcusvinicius.767.mv@gmail.com>
2022-09-22 08:48:55 -0300

    Criado def para salvar um txt com as informações dos clientes

--- utils/helper.py
+++ utils/helper.py
@@ -1,4 +1,5 @@
 from datetime import date, datetime
+from models.cliente import *
 
 
 def date_for_str(data: date) -> str:
@@ -11,3 +12,9 @@ def str_for_date(data: str) -> date:
 
 def format_float_str_moeda(valor: float) -> str:
     return f'R$ {valor:,.2f}'
+
+
+def armazenar_dados(cliente: object):
+    dados = open(f'dados_usuarios/{cliente.nome}.txt', 'w', encoding='utf-8')
+    dados.write(f'{cliente.__str__()}')
+    dados.close()
