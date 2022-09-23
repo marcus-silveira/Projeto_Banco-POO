cec5e5564c76f44390a241d4426b5c5ba3e0525e
Marcus <marcusvinicius.767.mv@gmail.com>
2022-09-23 14:41:13 -0300

    Finalizadas as funções para tratamentode erros: nome, cpf, data de nascimento

--- utils/helper.py
+++ utils/helper.py
@@ -6,12 +6,7 @@ def date_for_str(data: date) -> str:
 
 
 def str_for_date(data: str) -> date or bool:
-    try:
-        return datetime.strptime(data, '%d/%m/%Y')
-    except AttributeError:
-        return False
-    except ValueError:
-        return False
+    return datetime.strptime(data, '%d/%m/%Y')
 
 
 def format_float_str_moeda(valor: float) -> str:
@@ -24,14 +19,17 @@ def armazenar_dados(cliente: object):
     dados.close()
 
 
-def tratar_informacoes(nome: str, cpf: str) -> list[str]:
+def tratar_nome(nome: str) -> str:
     nomes = []
     for i in nome.split():
         nomes.append(i.capitalize())
         nomes.append(' ')
     nome = ''
     nome = nome.join(nomes)
+    return nome
 
+
+def tratar_cpf(cpf: str) -> str:
     cpfs = []
     cpf = cpf.replace('.', '').replace('-', '')
     for i in cpf:
@@ -40,4 +38,28 @@ def tratar_informacoes(nome: str, cpf: str) -> list[str]:
     cpf = ''
     cpf = cpf.join(cpfs)
 
-    return [nome, cpf]
+    return cpf
+
+
+def tratar_data(data: str, arg) -> date or str:
+    try:
+        datetime.strptime(data, '%d/%m/%Y')
+        return str(data)
+    except ValueError:
+        print('\nData de nascimento inválida!\n'
+              'Use dia/mês/ano\n')
+        return arg()
+
+
+def confirmar_dados(conta: object):
+    print(f'{conta}')
+    resposta = input('\nAs informações estão corretas? S/N: ').upper()
+
+    if resposta == 'S':
+        return True
+    elif resposta == 'N':
+        print('\nTente novamente: ')
+        return False
+    else:
+        print('Escolha S/N para prosseguir: \n')
+        confirmar_dados(conta)
